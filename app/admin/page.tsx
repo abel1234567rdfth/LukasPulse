ca64e8076ced6b7bd0ed1ea59ba80f16abced7ed
Abel Guta <gutaabel546@gmail.com>
2025-11-10 14:08:46 +0300

    build error fix 4

--- app/admin/page.tsx
+++ app/admin/page.tsx
@@ -7,7 +7,7 @@ import React from "react";
 
 const Admin = async () => {
   const res = await fetch(
-    `${process.env.NEXT_PUBLIC_BASE_URL}/api/actions/appointmentactions/get-appointmentList`
+    `/api/actions/appointmentactions/get-appointmentList`
   );
   const appointments = await res.json();
 
